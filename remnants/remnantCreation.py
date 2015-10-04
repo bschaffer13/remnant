81e01291ad5261ec43238adeadaf08de6218f71a
Brandon <bschaffer13@gmail.com>
2015-10-04 17:54:21 -0600

    Commiting for the night

--- remnants/remnantCreation.py
+++ remnants/remnantCreation.py
@@ -4,4 +4,4 @@ from database.databaseController import DatabaseController
 def generate_random_remnant(level, types, rarity, database: DatabaseController, location=None):
 
     database.get_remnant_definitions_by_type(types, rarity)
-    
+
